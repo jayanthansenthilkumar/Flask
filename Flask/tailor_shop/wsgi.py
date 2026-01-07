6b84ce2723b99ce162d46049f378ddc1ba535071
jayanthansenthilkumar <jayanthansenthilkumar18@gmail.com>
2026-01-07 21:00:18 +0530

    Flask deleted

--- Flask/tailor_shop/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-WSGI config for tailor_shop project.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tailor_shop.settings')
-application = get_wsgi_application()
